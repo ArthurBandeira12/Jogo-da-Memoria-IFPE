8d55f924dd554b3fca58cfc9997cb498a424c3c1
ArthurBandeira12 <arthurbandeiralealdeoliveira@gmail.com>
2025-01-09 20:31:44 -0300

    jogo da memoria

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import "../about/aboutstyle.css"
+</script>
+
+
+
+<div id="about-page">
+    <!-- Cabeçalho com "Memory Match" no canto superior esquerdo -->
+    <header>
+        <div id="logo">
+            <span>MEMORY<br />MATCH</span>
+            <img src="/brain.png" alt="Brain Icon" />
+        </div>
+    </header>
+
+    <!-- Seção do time -->
+    <section id="team-section">
+        <h2>Our Team</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </section>
+
+    <!-- Cards da equipe -->
+    <section id="cards-container">
+        <div class="card">
+            <div class="card-image"><img src="./imagemprojeto.png" alt="imagemarthur"></div>
+            <div class="card-content">
+                <h3>Arthur Bandeira</h3>
+                <p>Tenho 22 anos, estou no primeiro periodo do curso de tsi.</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-image"></div>
+            <div class="card-content">
+                <h3>José Diego</h3>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Botão para voltar -->
+    <footer>
+        <a href="/" id="back-button">Back to Home</a>
+    </footer>
+</div>
